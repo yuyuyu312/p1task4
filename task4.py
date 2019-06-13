1e11d992f97c4602a868b79bf2256337ee63cb85
yuyuyu312 <yusupov.ilgiz2323@gmail.com>
2019-06-13 08:59:34 +0600

    p1task4

---
+++ task4.py
@@ -0,0 +1,21 @@
+
+#Make sure to un-comment the function line below when you are done.
+#Remember to name your function correctly.
+def last_letter(x):
+    a = len(x)
+    a = a - 1
+    return x[a]
+#print(last_letter("sdfasdfasdf"))
+#last_letter('hello world')
+
+#DO NOT remove lines below here,
+#this is designed to test your code.
+def test_last_letter():
+  assert last_letter('hello!') == '!'
+  assert last_letter('banana') == 'a'
+  assert last_letter('8') == '8'
+  assert last_letter('funnyguys') == 's'
+  assert last_letter('101') == '1'
+  print("YOUR CODE IS CORRECT!")
+#test your code by un-commenting the line(s) below
+test_last_letter()
